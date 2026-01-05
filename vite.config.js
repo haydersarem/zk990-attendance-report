69de99dd350a0df6c448951b1c39c26d6a44c491
Hayder Sarem <haidersarim@gmail.com>
2026-01-05 23:54:15 +0300

    إصلاح خطأ بناء Tailwind v4 باستخدام إضافة Vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite' // استيراد التدوينة
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(), // إضافة التدوينة هنا
+  ],
 })
